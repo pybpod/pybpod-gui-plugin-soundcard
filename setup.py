2f935cd09b3dc9df18209744ce628370d09dd390
Luís Teixeira <micboucinha@gmail.com>
2019-11-07 13:48:45 +0000

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["libusb", "pyusb", "aenum"]
 
 setup(
     name="pybpod-gui-plugin-soundcard",
-    version="0.1.5",
+    version='0.1.6',
     description="""PyBpod Sound card module""",
     long_description="""Library to control the Harp Sound Card board developed by the Scientific Hardware Platform at
     the Champalimaud Foundation.""",
